a0894f009d41f9a2ad0b2e3423ead07e6164d2de
sdk-team <sdk-team@alibabacloud.com>
2020-07-22 16:01:02 +0800

    Generated 2019-02-25 for sdk-test.

---
+++ cpp/output/d0a2b216-571c-4b9f-a0b3-b195a853e678/publish.sh
@@ -0,0 +1,1 @@
+java -jar /Users/yangdengyu/portal-cluster/tools/sdk.generator-1.0.0-SNAPSHOT-jar-with-dependencies.jar --from-dir cpp sdk-test 2019-02-25 public openAPI /Users/yangdengyu/portal-cluster/tools/pop-meta/sdk-test output/d0a2b216-571c-4b9f-a0b3-b195a853e678 patch "" 
---
+++ cpp/output/d0a2b216-571c-4b9f-a0b3-b195a853e678/sdk-test/include/alibabacloud/sdk-test/Sdk_testClient.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2009-2017 Alibaba Cloud All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ALIBABACLOUD_SDK_TEST_SDK_TESTCLIENT_H_
+#define ALIBABACLOUD_SDK_TEST_SDK_TESTCLIENT_H_
+
+#include <future>
+#include <alibabacloud/core/AsyncCallerContext.h>
+#include <alibabacloud/core/EndpointProvider.h>
+#include <alibabacloud/core/RpcServiceClient.h>
+#include "Sdk_testExport.h"
+#include "model/DescribeRegionsRequest.h"
+#include "model/DescribeRegionsResult.h"
+
+
+namespace AlibabaCloud
+{
+	namespace Sdk_test
+	{
+		class ALIBABACLOUD_SDK_TEST_EXPORT Sdk_testClient : public RpcServiceClient
+		{
+		public:
+			typedef Outcome<Error, Model::DescribeRegionsResult> DescribeRegionsOutcome;
+			typedef std::future<DescribeRegionsOutcome> DescribeRegionsOutcomeCallable;
+			typedef std::function<void(const Sdk_testClient*, const Model::DescribeRegionsRequest&, const DescribeRegionsOutcome&, const std::shared_ptr<const AsyncCallerContext>&)> DescribeRegionsAsyncHandler;
+
+			Sdk_testClient(const Credentials &credentials, const ClientConfiguration &configuration);
+			Sdk_testClient(const std::shared_ptr<CredentialsProvider> &credentialsProvider, const ClientConfiguration &configuration);
+			Sdk_testClient(const std::string &accessKeyId, const std::string &accessKeySecret, const ClientConfiguration &configuration);
+			~Sdk_testClient();
+			DescribeRegionsOutcome describeRegions(const Model::DescribeRegionsRequest &request)const;
+			void describeRegionsAsync(const Model::DescribeRegionsRequest& request, const DescribeRegionsAsyncHandler& handler, const std::shared_ptr<const AsyncCallerContext>& context = nullptr) const;
+			DescribeRegionsOutcomeCallable describeRegionsCallable(const Model::DescribeRegionsRequest& request) const;
+	
+		private:
+			std::shared_ptr<EndpointProvider> endpointProvider_;
+		};
+	}
+}
+
+#endif // !ALIBABACLOUD_SDK_TEST_SDK_TESTCLIENT_H_
